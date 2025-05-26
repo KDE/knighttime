255d8b54e92e065b381724b81cd7451aa2cea0bc
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-05-27 00:13:43 +0300

    Add license tags to Messages.sh files

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,6 @@
 #! /usr/bin/env bash
+#
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
+
 $XGETTEXT `find . -not -path "./daemon/*" -name \*.cpp` -o $podir/knighttime.pot
--- src/daemon/Messages.sh
+++ src/daemon/Messages.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
+#
+# SPDX-FileCopyrightText: None
+# SPDX-License-Identifier: CC0-1.0
+
 $EXTRACTRC `find . -name \*.kcfg` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.cpp` -o $podir/knighttimed.pot
 rm -f rc.cpp
